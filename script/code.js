f6c22f50d8e2c1d7345ff18b31ec6759967926ac
Name <anushqakandan03@gmail.com>
2024-05-14 14:08:20 +0200

    Created Code

---
+++ script/code.js
@@ -0,0 +1,13 @@
+
+let firstName = 'Anushqa'
+let lastName = 'Kandan'
+console.log(firstName+" "+ lastName);
+
+let numberVar = 5
+console.log(numberVar*5);
+
+// Output = 25
+
+const name = 'Anushqa';
+const name = 'Kandan';
+// Yes, output = TypeError: name has already been declared
